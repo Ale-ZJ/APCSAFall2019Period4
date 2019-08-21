0144455ad67008f4ac477b1b92e8fca9d648c187
APCS2nd <APCS2nd@MHS-LT9-01>
2019-08-21 12:17:17 -0700

    complete Hello world project

---
+++ Hello_World_Project/src/HelloWorld.java
@@ -0,0 +1,15 @@
+//Alexandra Zhang Jiang
+//Aug 16, 2019
+//First class in JAVA for APCS A
+//Prints a message to the screen
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.print("Hello World! ");
+		System.out.println("Hello Vivian!");
+		System.out.print("From Alexandra :)");
+
+	}
+
+}
