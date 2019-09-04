5f3143625a4b88756ee3551c98ccc3c0cdc524a9
APCS2nd <APCS2nd@MHS-LT9-01>
2019-09-04 12:02:49 -0700

    part 1 (up to discriminant) code of Access Calculate

---
+++ Access Calculate/src/DoMath.java
@@ -0,0 +1,12 @@
+//Alexandra Zhang Jiang 
+//August 29
+//Call math methods from the calculate library I created
+//client code or runner code
+
+public class DoMath {
+
+	public static void main(String[] args) {
+		System.out.println(Calculate.square(-11));
+	}
+
+}
