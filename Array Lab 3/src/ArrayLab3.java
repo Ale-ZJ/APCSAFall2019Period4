afb84d1d37a976d83759ba3c27d728e46b438175
alexa <alexa@DESKTOP-S37KJ90>
2019-10-29 17:08:12 -0700

    finished array 3 lab

--- Array Lab 3/src/ArrayLab3.java
+++ Array Lab 3/src/ArrayLab3.java
@@ -2,11 +2,12 @@
 //October 24, 2019
 //Practice Array with a 3rd project!
 
-import java.util.*;
+import java.util.Arrays;
 
 public class ArrayLab3 {
 
 	public static void main(String[] args) {
+		//initialize variables
 		int[] a1 = {5, 10, 15, 20, 25, 30, 35, 40};
 		int[] a2 = {7, 14, 21, 28, 35, 42, 49, 56};
 		
@@ -17,10 +18,12 @@ public class ArrayLab3 {
 		int[] appendArr = append(a1, appendNum);
 		int[] removeArr = remove(a2, removeIdx);
 		
-		
 		int sumOfEvens = 0;
 		sumOfEvens = sumEven(appendArr);
+		
 		rotateRight(a1);
+		
+		//print results
 		System.out.println("Sum of a1 & a2: " + Arrays.toString(sumArr));
 		System.out.println("Appended array: " + Arrays.toString(appendArr));
 		System.out.println("Shortened array: " + Arrays.toString(removeArr));
@@ -32,31 +35,60 @@ public class ArrayLab3 {
 	//arr1 and arr2 have at least one element and are the same length.
 	public static int[] sum(int[] arr1, int[] arr2) {
 		int[] sumArray = new int[arr1.length];
+		for (int i = 0; i < sumArray.length; i++) {
+			sumArray[i] = arr1[i] + arr2[i];
+		}
 		return sumArray;
 	}
 
 	//Gives an array with num appended at the end of arr.  
 	//Assume array arr has at least one element.
 	public static int[] append(int[] arr, int num) {
-		
+		int[] appendedArray = Arrays.copyOf(arr, arr.length + 1);
+		appendedArray[arr.length] = num;
+		return appendedArray;
 	}
 	
 	//array of integers consisting of all of the elements of arr except for the element at index idx
 	//You can assume arr has at least two elements.
 	public static int[] remove(int[] arr, int idx) {
+		int[] beforeIdx = Arrays.copyOfRange(arr, 0, idx);
+		int[] afterIdx = Arrays.copyOfRange(arr, idx + 1, arr.length);
+		int[] removedArray = new int[beforeIdx.length + afterIdx.length];
 		
+		int index = 0;
+		
+		for (int element : beforeIdx) {
+			removedArray[index] = element;
+			index++;
+		}
+		
+		for (int element : afterIdx) {
+			removedArray[index] = element;
+			index++;
+		}
+		
+		return removedArray;
 	}
 	
 	//returns an integer containing the sum of the elements at the even indices of arr.  
 	//You can assume arr has at least one element.
 	public static int sumEven(int[] arr) {
-		
+		int evenSum = 0;
+		for (int i = 0; i < arr.length; i += 2) {
+			evenSum += arr[i];
+		}
+		return evenSum;
 	}
 	
 	//The rotateRight method moves each element of arr one index to the right (element 0 goes to element 1, element 1 goes to element 2, …, element n-1 goes to element 0).  
 	//You can assume arr has at least one element.
 	public static void rotateRight(int[] arr) {
-		
+		int lastElement = arr[arr.length - 1];			//stores temporarily last element
+		for (int i = arr.length - 1; i > 0; i--) {
+			arr[i] = arr[i - 1];
+		}
+		arr[0] = lastElement;
 	}
 
 }
